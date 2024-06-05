b2a2eea99a7c5a404467090739b2cc3b0f967c78
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2024-06-05 14:21:03 -0300

    Delete python directory

--- python/bubble-sort.py
+++
@@ -1,8 +0,0 @@
-def bubbleSort(v):
-    for i in range(0, n):
-        for j in range(i, n-1):
-            if v[j] > v[j+1]:
-                aux = v[j]
-                v[j] = v[j+1]
-                v[j+1] = aux
-                
